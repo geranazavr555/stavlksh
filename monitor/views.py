eefc0a1cb4d4b06763ded7cc1299c4a6df8e895b
Georgiy Nazarov <geranazavr555@yandex.ru>
2017-07-08 21:03:02 +0300

    Add caching to monitor-view

--- monitor/views.py
+++ monitor/views.py
@@ -1,9 +1,26 @@
+from datetime import datetime
 from django.shortcuts import render
 
 from . import scraper
 
 
+class Cache:
+    def __init__(self, function_to_get_data, time_to_live):
+        self.func = function_to_get_data
+        self.date = datetime.min
+        self.ttl = time_to_live
+        self.data = None
+
+    def __call__(self, *args, **kwargs):
+        if (datetime.now() - self.date).total_seconds() > self.ttl or (self.data is None):
+            self.data = self.func(*args, **kwargs)
+            self.date = datetime.now()
+        return self.data
+
+get_summary_results = Cache(scraper.get_summary_results, 60)
+
+
 def home(request):
-    contests, contestants = scraper.get_summary_results(91, 94, 96, 98, 101, 103, 105, 107, 109, 112, 113, 116)
+    contests, contestants = get_summary_results(91, 94, 96, 98, 101, 103, 105, 107, 109, 112, 113, 116)
     context = {"contestants": contestants, "contests": contests}
     return render(request, "monitor/summary_monitor.html", context)
