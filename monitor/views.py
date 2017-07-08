94b6d22dce6414b861f8eb2a7eb389f1af382e50
Georgiy Nazarov <geranazavr555@yandex.ru>
2017-07-08 14:41:36 +0300

    Add draft version of html-generating and responding

--- monitor/views.py
+++ monitor/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from . import scraper
 
 
 def home(request):
-    return HttpResponse("ok")
+    contests, contestants = scraper.get_summary_results(91, 94, 96, 98, 101, 103, 105, 107, 109, 112, 113, 116)
+    context = {"contestants": contestants, "contests": contests}
+    return render(request, "monitor/summary_monitor.html", context)
