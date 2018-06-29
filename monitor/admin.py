2f3bcb5643c37ffe74a5ef759398262757a7b092
Georgiy Nazarov <geranazavr555@yandex.ru>
2018-06-29 15:27:05 +0300

    Improve admin interface

--- monitor/admin.py
+++ monitor/admin.py
@@ -2,20 +2,29 @@ from django.contrib import admin
 
 from . import models
 
+class MonitorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+admin.site.register(models.Monitor, MonitorAdmin)
+
 
 class CachedHtmlAdmin(admin.ModelAdmin):
-    list_display = ("id", "content", "date")
+    list_display = ("id", "content", "date", "owner")
+
 
 admin.site.register(models.CachedHtml, CachedHtmlAdmin)
 
 
 class MonitoredContestAdmin(admin.ModelAdmin):
-    list_display = ("stavpoisk_id", )
+    list_display = ("stavpoisk_id", "owner")
+
 
 admin.site.register(models.MonitoredContest, MonitoredContestAdmin)
 
 
 class ExcludedUserAdmin(admin.ModelAdmin):
-    list_display = ("name", )
+    list_display = ("name", "owner")
+
 
 admin.site.register(models.ExcludedUser, ExcludedUserAdmin)
--- monitor/models.py
+++ monitor/models.py
@@ -4,24 +4,36 @@ from django.db import models
 class Monitor(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return "Сводный монитор: " + self.name
+
 
 class CachedHtml(models.Model):
     content = models.TextField()
     date = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(Monitor,
                               on_delete=models.CASCADE,
-                              verbose_name="Monitor")
+                              verbose_name="Monitor",
+                              null=False,
+                              blank=False)
 
 
 class MonitoredContest(models.Model):
     stavpoisk_id = models.IntegerField()
     owner = models.ForeignKey(Monitor,
                               on_delete=models.CASCADE,
-                              verbose_name="Monitor")
+                              verbose_name="Monitor",
+                              null=False,
+                              blank=False)
+
+    def __str__(self):
+        return "Stavpoisk contest, id " + str(self.stavpoisk_id)
 
 
 class ExcludedUser(models.Model):
     name = models.CharField(max_length=255)
     owner = models.ForeignKey(Monitor,
                               on_delete=models.CASCADE,
-                              verbose_name="Monitor")
+                              verbose_name="Monitor",
+                              null=False,
+                              blank=False)
