e9f7f4d7503394c2a7a2c9e31fd5680d4a7b233e
Georgiy Nazarov <geranazavr555@yandex.ru>
2018-06-29 14:52:44 +0300

    Change db architecture to be able to support plural monitors

--- monitor/models.py
+++ monitor/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 
 
+class Monitor(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class CachedHtml(models.Model):
     content = models.TextField()
     date = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(Monitor,
+                              on_delete=models.CASCADE,
+                              verbose_name="Monitor")
 
 
 class MonitoredContest(models.Model):
     stavpoisk_id = models.IntegerField()
+    owner = models.ForeignKey(Monitor,
+                              on_delete=models.CASCADE,
+                              verbose_name="Monitor")
 
 
 class ExcludedUser(models.Model):
     name = models.CharField(max_length=255)
+    owner = models.ForeignKey(Monitor,
+                              on_delete=models.CASCADE,
+                              verbose_name="Monitor")
