698c47d79265de68a7cf2d37c687bf53d3c233a0
Georgiy Nazarov <geranazavr555@yandex.ru>
2018-06-29 16:29:00 +0300

    Add plural monitor landing

--- monitor/views.py
+++ monitor/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render
 
 from . import scraper
 from . import models
@@ -6,7 +7,11 @@ from . import models
 
 def exact_monitor(request, monitor_id):
     try:
-        html = models.CachedHtml.objects.get(id=int(monitor_id)).content
+        html = models.CachedHtml.objects.get(owner_id=int(monitor_id)).content
         return HttpResponse(html)
     except models.CachedHtml.DoesNotExist:
         return HttpResponseBadRequest("No such monitor: id = " + monitor_id)
+
+
+def home(request):
+    return render(request, "monitor/landing.html", {"monitors" : models.Monitor.objects.all()})
